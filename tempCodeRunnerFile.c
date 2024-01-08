510bb3752f986177411afd968a4e7623acafaccc
Ajay23052004 <210303105063@paruluniversity.ac.in>
2024-01-08 09:17:25 +0530

    min_stcak_code_upload

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,21 @@
-  // printf("Enter Size of Stack:");
-    // scanf("%d",&size);
+int n,val;
+    // while(true){
+    // printf("--------------------------Menu----------------------");
+    // printf("\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
+    // scanf("%d",&n);
+    // switch(n){
+    //     case 1: printf("Enter value To push:");
+    //             scanf("%d",&val);
+    //             push(val);
+    //     break;
+    //     case 2: 
+    //             pop();
+    //     break;
+    //     case 3:display();
+    //     break;
+    //     case 4:exit(0);
+    //     break;
+    //     default:printf("Invalid input");
+    //     break;
+    // }
+    // }
