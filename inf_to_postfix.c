c22c4ae76f87fbdfae077c9f4ef22d6c42c19ae9
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-01-19 08:53:34 +0530

    upload

---
+++ inf_to_postfix.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int top=-1;
+char exp[100];
+void push(){
+    if(top==size-1)
+}
+int main(){
+
+ 
+  printf("Enter Your Infix Expression to Convert PostFix: ");
+  scanf("%s",exp);
+  //find length of given input
+  int len=strlen(exp);
+  
+   
+   
+
+}
